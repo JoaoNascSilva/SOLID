15042f385e7b320bd574fadaa39947f9a5aa6fbf
JoaoSilva <joao.antonio.89@gmail.com>
2020-07-17 18:40:10 -0300

    Solid - SRP

---
+++ SOLID/SRP/CustomerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SRP
+{
+    public class CustomerService
+    {
+
+        public string AddCustomer(Customer customer)
+        {
+            if (!customer.Validar())
+                return "Dados invalidos.";
+
+            var repository = new CustomerRepository();
+            repository.Add(customer);
+
+            return "Cliente cadastrado com sucesso.";
+        }
+
+        public string DeleteCustomer(Customer customer)
+        {
+            var repository = new CustomerRepository();
+            repository.Delete(customer);
+
+            return "Cliente removido.";
+        }
+
+    }
+}
---
+++ SOLID/SRP/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace SRP
+{
+    public class Entity
+    {
+        public string Id { get; private set; }
+
+        public Entity()
+        {
+            this.Id = System.Guid.NewGuid().ToString().Substring(0, 8);
+        }
+    }
+}
