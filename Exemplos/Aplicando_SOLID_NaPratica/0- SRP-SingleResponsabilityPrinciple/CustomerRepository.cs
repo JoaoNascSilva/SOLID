72fb6844a4695f221fc9a58b99e69f2a7131379e
João Silva <34160990+JoaoNascSilva@users.noreply.github.com>
2020-07-29 15:27:14 -0300

    Create CustomerRepository.cs
    Adicionado CustomerRepository para melhor entendimento do SRP.

---
+++ Exemplos/Aplicando_SOLID_NaPratica/0- SRP-SingleResponsabilityPrinciple/CustomerRepository.cs
@@ -0,0 +1,24 @@
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Aplicando_SOLID_NaPratica
+{
+    public class CustomerRepository
+    {
+        public void AddCustomer()
+        {
+            using(var connection = new SqlConnection())
+            {
+                var command = new SqlCommand();
+                
+                connection.ConnectionString = "MinhaConexao";
+                command.Connection = connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "[ SQL INSERT ] ";
+                
+                connection.Open();
+                connection.ExecuteNonQuery();                
+            }
+        }
+    }
+}
